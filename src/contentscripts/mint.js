27fec63f62107a5d1f3c5c4793b237d87210ec12
Joe Levy <jodoglevy@gmail.com>
2015-02-07 14:15:36 -0800

    adding more mint fields

--- src/contentscripts/mint.js
+++ src/contentscripts/mint.js
@@ -18,15 +18,32 @@ function startMint() {
         mintInfo.creditScore = parseInt(creditScoreString);
       }
 
-      // get marital status from mint
+      // get about me information from mint
       $.ajax({
         url: "https://wwws.mint.com/htmlFragment.xevent?task=as-nav-content-about&rnd=" + (Math.random() * 100000000000000000)
       })
       .done(function(response) {
         var aboutMeHtml = $(response.xmlContent);
+        
+        var ageRange = aboutMeHtml.find("#as-nav-content-about-age option:selected").text();
+        var gender = aboutMeHtml.find("#as-nav-content-about-male").parent().find("input:checked").val();
         var maritalStatus = aboutMeHtml.find("#as-nav-content-about-martial option:selected").text();
+        var educationLevel = aboutMeHtml.find("#as-nav-content-about-education option:selected").text();
+        var profession = aboutMeHtml.find("#as-nav-content-about-profession option:selected").text();
+        var incomeRange = aboutMeHtml.find("#as-nav-content-about-income option:selected").text();
+        var residentialStatus = aboutMeHtml.find("#as-nav-content-about-residential option:selected").text();
+        var householdSizeAdults = aboutMeHtml.find("#as-nav-content-about-household-adults option:selected").text().split("")[0];
+        var householdSizeChildren = aboutMeHtml.find("#as-nav-content-about-household-children option:selected").text().split("")[0];
       
+        mintInfo.ageRange = ageRange;
+        mintInfo.gender = gender;
         mintInfo.maritalStatus = maritalStatus;
+        mintInfo.educationLevel = educationLevel;
+        mintInfo.profession = profession;
+        mintInfo.incomeRange = incomeRange;
+        mintInfo.residentialStatus = residentialStatus;
+        mintInfo.householdSizeAdults = householdSizeAdults;
+        mintInfo.householdSizeChildren = householdSizeChildren;
 
         console.log(mintInfo);
         globalStorage.setItem("mintInfo", JSON.stringify(mintInfo));
