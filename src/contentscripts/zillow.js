74724b0dd154210fc53087b5af5a50cd2f9dec3d
stephenh0312 <stephen_j_han@hotmail.com>
2015-02-07 19:00:51 -0800

    Add HUD check for WA state

--- src/contentscripts/zillow.js
+++ src/contentscripts/zillow.js
@@ -8,7 +8,6 @@ function startZillow() {
     });
   }, 100);
 
-  setTimeout(function() {
     globalStorage.getItem("mintInfo", function(value) {
 	console.log("Value: " + value);
 
@@ -33,18 +32,71 @@ function startZillow() {
 	console.log("Loan Amount based on current monthlyHousingCost and avg monthly leftOver: " + maxLoanAmount);
 	
 	var availableForDown = totalLiquidAssets*.9;
-	var comfortableHouseCost = currentHouseCostLoanAmount + availableForDown;
-	var maxHouseCost = maxLoanAmount + availableForDown;
+	var comfortableHouseCost = Math.round(currentHouseCostLoanAmount + availableForDown);
+	var maxHouseCost = Math.round(maxLoanAmount + availableForDown);
 
 	console.log("Comfortable House cost: " + comfortableHouseCost);
 	console.log("Max House cost: " + maxHouseCost);
 
-	getInterestRate();
+	var interestRate = getInterestRate(comfortableHouseCost, function(interestRate) {
+			console.log("interestRate: " + interestRate);
+	});
     });
-  }, 1000);
 }
 
-function getInterestRate() {
+function doIqualify(state) {
+	globalStorage.getItem("userInfo", function(value) {
+		switch(state) {
+			case "WA":
+			case "Washington":
+			case "washington":
+				return doIQualifyInWashington(value);
+				break;
+			default:
+				break;
+		}
+	});
+}
+
+function doIqualifyInWashington(value) {
+	var parsedJSON = JSON.parse(value);
+	var isVeteran = parsedJSON.isVeteran;
+	var isTeacher = parsedJSON.isTeacher;
+	var isDisabled = parsedJSON.isDisabled;
+	var householdSizeAdults = parsedJSON.householdSizeAdults;
+	var householdSizeChildren = parsedJSON.householdSizeChildren;
+	var totalInHousehold = householdSizeAdults+householdSizeChildren;
+	var monthlyIncome = parsedJSON.monthlyIncome;
+	var totalIncome = monthlyIncome * 12;
+
+	if (totalIncome < 97000)
+		return true;
+	if (isVeteran && totalIncome < 88000)
+		return true;
+	if (isTeacher && totalIncome < 88000)
+		return true;
+	if (isDisabled && totalIncome < 88000)
+		return true;
+	if (totalInHousehold == 1 && totalIncome < 44000)
+		return true;
+	if (totalInHousehold == 2 && totalIncome < 51000)
+		return true;
+	if (totalInHousehold == 3 && totalIncome < 57000)
+		return true;
+	if (totalInHousehold == 4 && totalIncome < 63000)
+		return true;
+	if (totalInHousehold == 5 && totalIncome < 69000)
+		return true;
+	if (totalInHousehold == 6 && totalIncome < 74000)
+		return true;
+	if (totalInHousehold == 7 && totalIncome < 79000)
+		return true;
+	if (totalInHousehold == 8 && totalIncome < 84000)
+		return true;
+return false;
+}
+
+function getInterestRate(comfortableHouseCost, callback) {
 	var cookie = document.cookie;
 	var guidCookie = getCookie("zguid");
 	var decodeGuid = decodeURIComponent(guidCookie);
@@ -54,27 +106,61 @@ function getInterestRate() {
 	console.log("guid: " + parsedGUIDArray[0]);
 
 	var guid = parsedGUIDArray[0];
-	var submitRequestUrl = 'https://mortgageapi.zillow.com/submitRequest?property.type=SingleFamilyHome&property.use=Primary&property.zipCode=98112&property.value=1315450&borrower.creditScoreRange=R_760_&borrower.annualIncome=70000&purchase.downPayment=263090&submittedForFeaturedLenders=true&desiredPrograms.0=Fixed30Year&partnerId=RD-FVBXMBZ&userSessionId=c57a4db8-2624-47ce-8471-f4937b89d535'
-	var submitRequestResponse = httpGet(submitRequestUrl);
-	var parsedSubmitRequestResponse = JSON.parse(submitRequestResponse);
-	var requestId = parsedSubmitRequestResponse.requestId;
-	console.log("Response: " + submitRequestResponse);
-	console.log("RequestId: " + requestId);
+	var submitRequestUrl = 'https://mortgageapi.zillow.com/submitRequest?' +
+		'property.type=SingleFamilyHome&' +
+		'property.use=Primary&' +
+		'property.zipCode=98112&' +
+		'property.value=' + comfortableHouseCost + '&'+
+		'borrower.creditScoreRange=R_760_&' +
+		'borrower.annualIncome=70000&' +
+		'purchase.downPayment=20090&' +
+		'submittedForFeaturedLenders=true&' +
+		'desiredPrograms.0=Fixed30Year&' +
+		'partnerId=RD-FVBXMBZ&' +
+		'userSessionId=' + guid
+	
+	console.log("SubmitRequestUrl: " + submitRequestUrl);
+	$.ajax({
+  		url: submitRequestUrl
+	}).done(function(submitRequestResponse) {
+		//var submitRequestResponse = httpGet(submitRequestUrl);
+		var requestId = submitRequestResponse.requestId;
+		console.log("Response: " + submitRequestResponse);
+		console.log("RequestId: " + requestId);
+		var getQuotesUrl = 'https://mortgageapi.zillow.com/getQuotes?partnerId=RD-FVBXMBZ&requestRef.id=' + requestId;
+		
+		$.ajax({
+  			url: getQuotesUrl
+		}).done(function(getQuotesResponse) {
+			console.log("GetQuotes Response: " + getQuotesResponse);
+			for (var key in getQuotesResponse) {
+	      			console.log(key,":",getQuotesResponse[key]);
+	  		}
+	
+			var quotes = getQuotesResponse["quotes"];
+			var count = 0;
+			var totalapr = 0;
+			for (quote in quotes) {
+				var curQuote = quotes[quote];
+				var apr = JSON.stringify(curQuote["apr"]);
+				console.log("APR: " + apr);
+				totalapr += parseInt(apr);
+				count++;
+			}
+		
+			if (count != 0) {
+				var avgapr = totalapr/count;
+				console.log("average apr: " + avgapr);
+				callback(avgapr);
+			} else {
+				callback(null);
+			}
+		});
+	});
 
-	setTimeout(function(){
-    		var getQuotesUrl = 'https://mortgageapi.zillow.com/getQuotes?partnerId=RD-FVBXMBZ&requestRef.id=' + requestId;
-		var getQuotesResponse = httpGet(getQuotesUrl);
-		console.log("GetQuotes Response: " + getQuotesResponse);
-		var parsedGetQuotesResponse = JSON.parse(getQuotesResponse);
-		var quotes = parsedGetQuotesResponse.quotes;
-		console.log("Quotes: " + quotes);
-		for (quote in quotes) {
-			console.log("APR: " + quote.apr);
-		}
-	}, 2000);
+	
 }
 
-// From W3 schools
 function getCookie(cname) {
     var name = cname + "=";
     var ca = document.cookie.split(';');
@@ -84,16 +170,5 @@ function getCookie(cname) {
         if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
     }
     return "";
-}
-
-// From Stack Overflow
-function httpGet(theUrl)
-{
-    var xmlHttp = null;
-
-    xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false );
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
 }
 
